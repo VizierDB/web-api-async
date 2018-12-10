3bd57569a7cb5b28e9613fe7c772f627ba1cbd94
Heiko Mueller <heiko.muller@gmail.com>
2018-12-10 15:52:58 -0500

    Add synchronous backend

---
+++ tests/backend/test_synchronous_backend.py
@@ -0,0 +1,189 @@
+"""Test the execute method of the synchronous backend."""
+
+import os
+import shutil
+import unittest
+
+from vizier.datastore.fs.base import FileSystemDatastore
+from vizier.engine.backend.synchron import SynchronousBackend
+from vizier.engine.packages.pycell.base import PACKAGE_PYTHON, PYTHON_CODE
+from vizier.engine.packages.pycell.processor import PyCellTaskProcessor
+from vizier.engine.packages.vizual.api.fs import DefaultVizualApi
+from vizier.engine.packages.vizual.base import PACKAGE_VIZUAL, VIZUAL_LOAD, VIZUAL_UPD_CELL
+from vizier.engine.packages.vizual.processor import VizualTaskProcessor
+from vizier.filestore.fs.base import DefaultFilestore
+
+import vizier.client.command.vizual as vizual
+import vizier.client.command.pycell as pycell
+import vizier.engine.packages.base as pckg
+
+
+
+SERVER_DIR = './.tmp'
+FILESTORE_DIR = './.tmp/fs'
+DATASTORE_DIR = './.tmp/ds'
+CSV_FILE = './.files/dataset.csv'
+
+DATASET_NAME = 'people'
+SECOND_DATASET_NAME = 'my_people'
+
+CREATE_DATASET_PY = """
+ds = vizierdb.new_dataset()
+ds.insert_column('Name')
+ds.insert_column('Age')
+ds.insert_row(['Alice', 23])
+ds.insert_row(['Bob', 34])
+ds = vizierdb.create_dataset('""" + SECOND_DATASET_NAME + """', ds)
+for row in ds.rows:
+    print row.get_value('Name')
+"""
+
+
+class TestSynchronousBackend(unittest.TestCase):
+
+    def setUp(self):
+        """Create an instance of the default vizier processor for an empty server
+        directory.
+        """
+        # Drop directory if it exists
+        if os.path.isdir(SERVER_DIR):
+            shutil.rmtree(SERVER_DIR)
+        os.makedirs(SERVER_DIR)
+        vizual = VizualTaskProcessor(api=DefaultVizualApi())
+        pycell = PyCellTaskProcessor()
+        self.backend = SynchronousBackend(
+            datastore=FileSystemDatastore(DATASTORE_DIR),
+            filestore=DefaultFilestore(FILESTORE_DIR),
+            processors={
+                PACKAGE_PYTHON: {PYTHON_CODE: pycell},
+                PACKAGE_VIZUAL: {
+                    VIZUAL_LOAD: vizual,
+                    VIZUAL_UPD_CELL: vizual
+                }
+            }
+        )
+
+    def tearDown(self):
+        """Clean-up by dropping the server directory.
+        """
+        if os.path.isdir(SERVER_DIR):
+            shutil.rmtree(SERVER_DIR)
+
+    def test_can_execute(self):
+        """Test the can execute method with different commands."""
+        self.assertTrue(
+            self.backend.can_execute(
+                vizual.load_dataset(
+                    dataset_name=DATASET_NAME,
+                    file_id={pckg.FILE_ID: '000'},
+                    validate=True
+                )
+            )
+        )
+        self.assertTrue(
+            self.backend.can_execute(
+                vizual.update_cell(
+                    dataset_name=DATASET_NAME,
+                    column=1,
+                    row=0,
+                    value=9,
+                    validate=True
+                )
+            )
+        )
+        self.assertTrue(
+            self.backend.can_execute(
+                pycell.python_cell(
+                    source=CREATE_DATASET_PY,
+                    validate=True
+                )
+            )
+        )
+        self.assertFalse(
+            self.backend.can_execute(
+                vizual.insert_row(
+                    dataset_name=DATASET_NAME,
+                    position=1,
+                    validate=True
+                )
+            )
+        )
+        self.assertFalse(
+            self.backend.can_execute(
+                vizual.drop_dataset(
+                    dataset_name=DATASET_NAME,
+                    validate=True
+                )
+            )
+        )
+
+    def test_execute(self):
+        """Test executing a sequence of supported commands."""
+        context = dict()
+        fh = self.backend.filestore.upload_file(CSV_FILE)
+        cmd = vizual.load_dataset(
+            dataset_name=DATASET_NAME,
+            file_id={pckg.FILE_ID: fh.identifier},
+            validate=True
+        )
+        result = self.backend.execute(command=cmd, context=context)
+        self.assertTrue(result.is_success)
+        context = result.datasets
+        cmd = vizual.update_cell(
+            dataset_name=DATASET_NAME,
+            column=1,
+            row=0,
+            value=9,
+            validate=True
+        )
+        result = self.backend.execute(command=cmd, context=context)
+        self.assertTrue(result.is_success)
+        self.assertNotEqual(context[DATASET_NAME], result.datasets[DATASET_NAME])
+        context = result.datasets
+        cmd = pycell.python_cell(
+            source=CREATE_DATASET_PY,
+            validate=True
+        )
+        result = self.backend.execute(command=cmd, context=context)
+        self.assertTrue(result.is_success)
+        self.assertEquals(context[DATASET_NAME], result.datasets[DATASET_NAME])
+        self.assertTrue(SECOND_DATASET_NAME in result.datasets)
+        context = result.datasets
+        cmd = vizual.update_cell(
+            dataset_name=SECOND_DATASET_NAME,
+            column=1,
+            row=0,
+            value=9,
+            validate=True
+        )
+        result = self.backend.execute(command=cmd, context=context)
+        self.assertTrue(result.is_success)
+        self.assertEquals(context[DATASET_NAME], result.datasets[DATASET_NAME])
+        self.assertNotEqual(context[SECOND_DATASET_NAME], result.datasets[SECOND_DATASET_NAME])
+
+    def test_execute_unsupported_command(self):
+        """Test executing commands that are not supported for synchronous
+        execution.
+        """
+        with self.assertRaises(ValueError):
+            self.backend.execute(
+                command=vizual.insert_row(
+                    dataset_name=DATASET_NAME,
+                    position=1,
+                    validate=True
+                ),
+                context=dict()
+            )
+
+        with self.assertRaises(ValueError):
+            self.backend.execute(
+                command=vizual.drop_dataset(
+                    dataset_name=DATASET_NAME,
+                    validate=True
+                ),
+                context=dict()
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ vizier/engine/backend/synchron.py
@@ -0,0 +1,110 @@
+# Copyright (C) 2018 New York University,
+#                    University at Buffalo,
+#                    Illinois Institute of Technology.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Backend for synchronized command execution. This class is promarily intended
+for test purposes. It can only be used to append a new module to a workflow
+but not to insert delete or replace modules in an existing workflow.
+
+The synchronized backend can also be used as a base class for the asynchronous
+backend.
+"""
+
+from vizier.engine.backend.base import VizierBackend
+from vizier.engine.task.base import TaskContext
+
+
+class SynchronousBackend(VizierBackend):
+    """Backend that only supports synchronous execution of tasks. All methods
+    that are related to asynchronous task execution remain uninplemented.
+    """
+    def __init__(self, datastore, filestore, processors):
+        """Initialize the datastore and filestore. The processors dictionary
+        contains the task engines for all commands that can be executed. This
+        is a dictionary of dictionaries that are keyed by the package
+        identifier. Each internal idictionary contains the task engines keyed
+        by the command identifier.
+
+        Parameters
+        ----------
+        datastore: vizier.datastore.base.Datastore
+            Datastore for the project that execute the task
+        filestore: vizier.filestore.Filestore
+            Filestore for the project that executes the task
+        processors: dict(dict(vizier.engine.packages.task.processor.TaskProcessor))
+            Dictionary of task processors for executable tasks that are keyed
+            by the pakage identifier and the command identifier
+        """
+        self.datastore = datastore
+        self.filestore = filestore
+        self.processors = processors
+
+    def can_execute(self, command):
+        """Test whether a given command can be executed in synchronous mode. If
+        the result is True the command can be executed in the same process as
+        the calling method.
+
+        Parameters
+        ----------
+        command : vizier.viztrail.command.ModuleCommand
+            Specification of the command that is to be executed
+
+        Returns
+        -------
+        bool
+        """
+        pckg = command.package_id
+        cmd = command.command_id
+        return pckg in self.processors and cmd in self.processors[pckg]
+
+    def execute(self, command, context, resources=None):
+        """Execute a given command. The command will be executed immediately if
+        the backend supports synchronous excution, i.e., if the .can_excute()
+        method returns True. The result is the execution result returned by the
+        respective package task processor.
+
+        Raises ValueError if the given command cannot be excuted in synchronous
+        mode.
+
+        Parameters
+        ----------
+        command : vizier.viztrail.command.ModuleCommand
+            Specification of the command that is to be executed
+        context: dict()
+            Dictionary of available resource in the database state. The key is
+            the resource name. Values are resource identifiers.
+        resources: dict(), optional
+            Optional information about resources that were generated during a
+            previous execution of the command
+
+        Returns
+        ------
+        vizier.engine.task.processor.ExecResult
+        """
+        if command.package_id in self.processors:
+            package = self.processors[command.package_id]
+            if command.command_id in package:
+                processor = package[command.command_id]
+                return processor.compute(
+                    command_id=command.command_id,
+                    arguments=command.arguments,
+                    context=TaskContext(
+                        datastore=self.datastore,
+                        filestore=self.filestore,
+                        datasets=context,
+                        resources=resources
+                    )
+                )
+        raise ValueError('cannot execute given command')
