d055434e08644ec3e7b622bd52ead569074b7113
Mike <mrb24@buffalo.edu>
2019-06-14 14:39:36 -0400

    bugfix and temporary hack to get spreadsheet working

--- vizier/datastore/mimir/reader.py
+++ vizier/datastore/mimir/reader.py
@@ -144,9 +144,9 @@ class MimirDatasetReader(DatasetReader):
                     col = self.columns[i]
                     col_index = self.col_map[col.name_in_rdb]
                     values[i] = row[col_index]
-                self.rows.append(DatasetRow(rowid_idx, values))
+                self.rows.append(DatasetRow(int(row_id.replace("'", "")), values))
             #self.rows.sort(key=lambda row: self.sortbyrowid(row.identifier))
-            self.rows.sort(key=lambda row: self.row_idxs[int(row.identifier)])
+            #self.rows.sort(key=lambda row: self.row_idxs[int(row.identifier)])
             self.read_index = 0
             self.is_open = True
         return self
